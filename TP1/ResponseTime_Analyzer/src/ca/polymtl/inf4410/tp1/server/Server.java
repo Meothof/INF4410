8bbe43ffd90523ccd2bd652905b389309fc0d3c8
Théo Moffelein <thmof@l4712-05.info.polymtl.ca>
2017-09-19 15:13:19 -0400

    generateclientid

--- TP1/ResponseTime_Analyzer/src/ca/polymtl/inf4410/tp1/server/Server.java
+++ TP1/ResponseTime_Analyzer/src/ca/polymtl/inf4410/tp1/server/Server.java
@@ -6,11 +6,16 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import ca.polymtl.inf4410.tp1.shared.ServerInterface;
 
 public class Server implements ServerInterface {
-
+	private int lastId;
 	public static void main(String[] args) {
 		Server server = new Server();
 		server.run();
@@ -18,6 +23,8 @@ public class Server implements ServerInterface {
 
 	public Server() {
 		super();
+		File ids = new File("ids.txt");
+		lastId =0;
 	}
 
 	private void run() {
@@ -54,5 +61,30 @@ public class Server implements ServerInterface {
 	public int execute(byte[] b) throws RemoteException {
 		return b.length;
 	}
+
+	public int generateClientId(){
+		FileOutputStream fos = null;
+		lastId++;
+		try {
+			fos = new FileOutputStream(new File("ids.txt"));
+			fos.write(lastId);
+		} catch (FileNotFoundException e) {
+
+		e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (fos != null)
+					fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	return lastId;
+
+	}
+
 
 }

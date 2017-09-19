0ef36f13a9f1d99745aa6602c82b4cec108e5bcf
Théo Moffelein <thmof@l4712-05.info.polymtl.ca>
2017-09-19 14:35:26 -0400

    q1

---
+++ TP1/ResponseTime_Analyzer/src/ca/polymtl/inf4410/tp1/client/FakeServer.java
@@ -0,0 +1,11 @@
+package ca.polymtl.inf4410.tp1.client;
+import java.util.Arrays;
+
+public class FakeServer {
+	int execute(int a, int b) {
+		return a + b;
+	}
+	int execute(byte[] b) {
+		return b.length;
+	}
+}
---
+++ TP1/ResponseTime_Analyzer/src/ca/polymtl/inf4410/tp1/shared/ServerInterface.java
@@ -0,0 +1,9 @@
+package ca.polymtl.inf4410.tp1.shared;
+import java.util.Arrays;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerInterface extends Remote {
+	int execute(int a, int b) throws RemoteException;
+	int execute(byte[] b) throws RemoteException;
+}

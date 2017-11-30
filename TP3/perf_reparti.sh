c061142126977402169a401dd87415a9ef306ac1
meothof <theo.moffelein@gmail.com>
2017-11-30 10:52:14 -0500

    netoyage du depot + ajout du script de test en mode reparti

---
+++ TP3/perf_reparti.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -m # Enable Job Control
+START=$(date +%s.%N)
+for i in `seq 40`; do # start 40 jobs in parallel
+  curl 172.15.109.15:8000 &
+echo "Start $i"
+done
+
+# Wait for all parallel jobs to finish
+while [ 1 ];
+do
+  fg 2> /dev/null;
+  [ $? == 1 ] && break;
+done
+
+END=$(date +%s.%N)
+DIFF=$(echo " $END - $START" | bc)
+echo "time : "
+echo $DIFF
